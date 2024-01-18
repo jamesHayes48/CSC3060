041f71055e1145a0851f34394eee44b16f56a7f1
DingDongReallyFast <jhpipboy@gmail.com>
2024-01-17 18:55:01 -0700

    Update 6.6.cpp

--- 6.6.cpp
+++ 6.6.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-double kineticEnergy(double mass, double velocity);
+int kineticEnergy(int mass, int velocity);
 
 int main() {
-	double objectMass = 0;
-	double objectVelocity = 0;
-	double objectKE = 0;
+	int objectMass = 0;
+	int objectVelocity = 0;
+	int objectKE = 0;
 
 	cout << "Enter object's mass in kilograms and velocity in meters per second.\n";
 	cin >> objectMass >> objectVelocity;
@@ -18,7 +18,7 @@ int main() {
 	return 0;
 }
 
-double kineticEnergy(double mass, double velocity) {
-	double objectKineticEnergy = (mass * (velocity * velocity)) / 2.0;
+int kineticEnergy(int mass, int velocity) {
+	int objectKineticEnergy = (mass * (velocity * velocity)) / 2;
 	return objectKineticEnergy;
 }
