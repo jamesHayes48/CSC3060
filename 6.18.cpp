594a9492c73872ccff423936ec17f225cd68d8f0
DingDongReallyFast <jhpipboy@gmail.com>
2024-01-19 16:09:32 -0700

    Update 6.18.cpp

--- 6.18.cpp
+++ 6.18.cpp
@@ -27,40 +27,53 @@ double calculatePaintCost(double paintPrice, int multiplier);
 void displayData(int gallons, int laborHours, double paintCost, double laborCharges, double totalCost);
 
 int main() {
+	// Limits for every function
 	int leastNumRoom = 1;
 	double leastPrice = 10.00;
 	int leastWallSpace = 0;
 	int oneHourLabor = 8;
 	double oneHourCost = 25.00;
 
+	// User input
 	int userRooms = 0;
 	double userPaintPrice = 0.0;
 	int userWallSpace = 0;
 
+	// User output
 	int gallonsOfPaint = 0;
 	int laborHours = 0;
 	double laborCost = 0.0;
 	double paintCost = 0.0;
 	double totalCost = 0.0;
 
+	// Prompt user for rooms, price of the paint, and wallspace
 	userRooms = numberOfRooms(leastNumRoom);
 	userPaintPrice = priceOfPaint(leastPrice);
 	userWallSpace = wallSpace(leastWallSpace, userRooms);
 
+	// Find multiplier for calculations
 	int userMultiplier = (int) userWallSpace / 110;
 	
+	// Calculate gallons, labor hours and cost, and paint cost
 	gallonsOfPaint = calculateGallons(userMultiplier);
 	laborHours = calculateLaborHour(oneHourLabor, userMultiplier);
 	laborCost = calculateLaborCost(oneHourCost, laborHours);
 	paintCost = calculatePaintCost(userPaintPrice, userMultiplier);
 
+	// Find total cost
 	totalCost = laborCost + paintCost;
 
+	// Display all data
 	displayData(gallonsOfPaint, laborHours, paintCost, laborCost, totalCost);
 
 	return 0;
 }
 
+/*
+Purpose: Ask user for number of rooms
+Parameters: least number of rooms
+Return: Integer number of rooms
+*/
 int numberOfRooms(int leastNum) {
 	bool validInput = false;
 	int inputRoom = 0;
@@ -77,6 +90,12 @@ int numberOfRooms(int leastNum) {
 	return inputRoom;
 }
 
+
+/*
+Purpose: Ask user for the price of paint
+Parameters: Least cost of paint
+Return: Price of user selected paint
+*/
 double priceOfPaint(double leastCost) {
 	bool validInput = false;
 	double inputPrice = 0.0;
@@ -93,6 +112,11 @@ double priceOfPaint(double leastCost) {
 	return inputPrice;
 }
 
+/*
+Purpose: Ask user for number of rooms
+Parameters: least number of rooms
+Return: Integer number of rooms
+*/
 int wallSpace(int leastWallSpace, int numberOfRooms) {
 	bool validInput = false;
 	int inputWallSpace = 0;
@@ -116,26 +140,51 @@ int wallSpace(int leastWallSpace, int numberOfRooms) {
 	return totalWallSpace;
 }
 
+/*
+Purpose: Ask user for number of rooms
+Parameters: least number of rooms
+Return: Integer number of rooms
+*/
 int calculateGallons(int multiplier) {
 	int gallonPerHour = 1 * multiplier;
 	return gallonPerHour;
 }
 
+/*
+Purpose: Ask user for number of rooms
+Parameters: least number of rooms
+Return: Integer number of rooms
+*/
 int calculateLaborHour(int laborHour, int multiplier) {
 	int totalHours = laborHour * multiplier;
 	return totalHours;
 }
 
+/*
+Purpose: Ask user for number of rooms
+Parameters: least number of rooms
+Return: Integer number of rooms
+*/
 double calculateLaborCost(double laborCostHour, int laborHours) {
 	double laborCost = laborCostHour * laborHours;
 	return laborCost;
 }
 
+/*
+Purpose: Ask user for number of rooms
+Parameters: least number of rooms
+Return: Integer number of rooms
+*/
 double calculatePaintCost(double paintPrice, int multiplier) {
 	double paintCost = paintPrice * multiplier;
 	return paintCost;
 }
 
+/*
+Purpose: Ask user for number of rooms
+Parameters: least number of rooms
+Return: Integer number of rooms
+*/
 void displayData(int gallons, int laborHours, double paintCost, double laborCharges, double totalCost) {
 	cout << "Number of gallons required: " << gallons << "\n";
 	cout << "Hours of labor required: " << laborHours << "\n";
