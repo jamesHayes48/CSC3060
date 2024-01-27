1be303a03e6ef14151f7f2d0f3da7fe9033d03eb
DingDongReallyFast <jhpipboy@gmail.com>
2024-01-27 15:40:20 -0700

    Almost finished, need to test

--- 7.5.cpp
+++ 7.5.cpp
@@ -15,27 +15,89 @@ using namespace std;
 // Constants
 const int NUMBER_OF_MONKEYS = 3;
 const int DAYS_OF_WEEK = 5;
+const int NON_NEGATIVE = 0;
 
 // Function Prototypes
 // Input for pounds of food eaten
-void poundsEaten(int foodEaten[][DAYS_OF_WEEK], int monkeys, int days);
+void poundsEaten(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days, int least);
+
+void findAverage(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days);
+
+void findLeastAmount(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days);
+
+void findMostAmount(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days);
 
 int main() {
-	int monkeysFed[NUMBER_OF_MONKEYS][DAYS_OF_WEEK] = { 0 };
+	double monkeysFed[NUMBER_OF_MONKEYS][DAYS_OF_WEEK] = { 0 };
 
+	poundsEaten(monkeysFed, NUMBER_OF_MONKEYS, DAYS_OF_WEEK, NON_NEGATIVE);
 
 	return 0;
 }
 
-void poundsEaten(int foodEaten[][DAYS_OF_WEEK], int monkeys, int days) {
-	int pounds = 0;
-	
+void poundsEaten(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days, int least) {
+	// Iterate through rows and columns for each monkey
 	for (int row = 0; row < monkeys; row++) {
 		for (int column = 0; column < days; column++) {
-			cout << "Enter pounds for monkey " << row + 1 << " on day " << column + 1 << endl;
-			if () {
-				foodEaten[row][column] = pounds;
+			int pounds = 0;
+			bool valid = false;
+
+			// Keep prompting for valid input
+			while (valid == false) {
+				cout << "Enter pounds for monkey " << row + 1 << " on day " << column + 1 << endl;
+				cin >> pounds;
+
+				if (pounds >= least) {
+					foodEaten[row][column] = pounds;
+					valid = true;
+				}
+				else {
+					cout << "Error, invalid input found, please enter a positive number\n";
+				}
 			}
 		}
 	}
+}
+
+void findAverage(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days) {
+	double totalPounds = 0.0;
+	double average = 0.0;
+
+	for (int row = 0; row < monkeys; row++) {
+		for (int column = 0; column < days; column++) {
+			totalPounds += foodEaten[row][column];
+		}
+	}
+
+	average = totalPounds / days;
+
+	cout << "An average of " << average << "lb was eaten by the monkeys per day" << endl;
+}
+
+void findLeastAmount(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days) {
+	double leastAmount = foodEaten[0][0];
+	
+	for (int row = 0; row < monkeys; row++) {
+		for (int column = 0; column < days; column++) {
+			if (foodEaten[row][column] < leastAmount) {
+				leastAmount = foodEaten[row][column];
+			}
+		}
+	}
+
+	cout << "The least amount of food eaten on a day was " << leastAmount << "lb" << endl;
+}
+
+void findMostAmount(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days) {
+	double mostAmount = foodEaten[0][0];
+
+	for (int row = 0; row < monkeys; row++) {
+		for (int column = 0; column < days; column++) {
+			if (foodEaten[row][column] < mostAmount) {
+				mostAmount = foodEaten[row][column];
+			}
+		}
+	}
+
+	cout << "The most amount of food eaten on a day was " << mostAmount << "lb" << endl;
 }
