1114b3df4ffd5d2552d3a48a8399c85270b9006d
DingDongReallyFast <jhpipboy@gmail.com>
2024-01-27 18:06:11 -0700

    Finished 7.5
    Done with graded assignments in 3060

--- 7.5.cpp
+++ 7.5.cpp
@@ -3,10 +3,9 @@ Name: James Hayes
 Class : CS 3060 - 002
 Project Name : 7.5 Programming Challenges
 
-Description : This program prompts the user to enter the total number of sales for each type of
-salsa.The program then prints out the total number of sales of each salsa type, and the total
-sales.The program also finds and prints the name and sales of both the highest and lowest selling
-salsas.
+Description : This program prompts the user to enter the amount of food eaten for each monkey for 
+5 days. It then finds and prints the average of food eaten, the least amount of food eaten by one
+monkey, and the most amount of food eaten by one monkey.
 */
 
 #include <iostream>
@@ -21,20 +20,39 @@ const int NON_NEGATIVE = 0;
 // Input for pounds of food eaten
 void poundsEaten(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days, int least);
 
+// Find and print average of food eaten
 void findAverage(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days);
 
+// Find and print least amount of food eaten
 void findLeastAmount(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days);
 
+// Find and print most amount of food eaten
 void findMostAmount(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days);
 
 int main() {
+	// Array for storing pounds of food eaten
 	double monkeysFed[NUMBER_OF_MONKEYS][DAYS_OF_WEEK] = { 0 };
 
+	// Input user for pounds of food eaten
 	poundsEaten(monkeysFed, NUMBER_OF_MONKEYS, DAYS_OF_WEEK, NON_NEGATIVE);
 
+	// Find and print average 
+	findAverage(monkeysFed, NUMBER_OF_MONKEYS, DAYS_OF_WEEK);
+
+	// Find and print least amount of food eaten
+	findLeastAmount(monkeysFed, NUMBER_OF_MONKEYS, DAYS_OF_WEEK);
+
+	// Find and print most amount of food eaten
+	findMostAmount(monkeysFed, NUMBER_OF_MONKEYS, DAYS_OF_WEEK);
 	return 0;
 }
 
+/*
+Purpose: Input user for pounds of food eaten
+Parameters: Array of pounds of food eaten, number of monkeys, number of days, and 
+least amount of that can be eaten
+Return: None, but modify array of pounds of food eaten
+*/
 void poundsEaten(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days, int least) {
 	// Iterate through rows and columns for each monkey
 	for (int row = 0; row < monkeys; row++) {
@@ -59,6 +77,11 @@ void poundsEaten(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days, int le
 	}
 }
 
+/*
+Purpose: Find and print average of food eaten
+Parameters: Array of pounds of food eaten, number of monkeys, number of days
+Return: None, but find and print average of food eaten
+*/
 void findAverage(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days) {
 	double totalPounds = 0.0;
 	double average = 0.0;
@@ -69,11 +92,16 @@ void findAverage(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days) {
 		}
 	}
 
-	average = totalPounds / days;
+	average = totalPounds / (days * monkeys);
 
-	cout << "An average of " << average << "lb was eaten by the monkeys per day" << endl;
+	cout << "An average of " << average << " pounds was eaten by the monkeys per day" << endl;
 }
 
+/*
+Purpose: Find and print least amount of food eaten
+Parameters: Array of pounds of food eaten, number of monkeys, number of days
+Return: None, but find and print least amount of food eaten
+*/
 void findLeastAmount(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days) {
 	double leastAmount = foodEaten[0][0];
 	
@@ -85,19 +113,24 @@ void findLeastAmount(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days) {
 		}
 	}
 
-	cout << "The least amount of food eaten on a day was " << leastAmount << "lb" << endl;
+	cout << "The least amount of food eaten on a day was " << leastAmount << " pounds" << endl;
 }
 
+/*
+Purpose: Find and print most amount of food eaten
+Parameters: Array of pounds of food eaten, number of monkeys, number of days
+Return: None, but find and print most amount of food eaten
+*/
 void findMostAmount(double foodEaten[][DAYS_OF_WEEK], int monkeys, int days) {
 	double mostAmount = foodEaten[0][0];
 
 	for (int row = 0; row < monkeys; row++) {
 		for (int column = 0; column < days; column++) {
-			if (foodEaten[row][column] < mostAmount) {
+			if (foodEaten[row][column] > mostAmount) {
 				mostAmount = foodEaten[row][column];
 			}
 		}
 	}
 
-	cout << "The most amount of food eaten on a day was " << mostAmount << "lb" << endl;
+	cout << "The most amount of food eaten on a day was " << mostAmount << " pounds" << endl;
 }
