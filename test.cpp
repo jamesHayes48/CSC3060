98dd07b18307c0ae6fa5bb8cf23225051fec91dc
DingDongReallyFast <jhpipboy@gmail.com>
2024-03-10 14:30:27 -0600

    Test

---
+++ test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+enum Tree {OAK, MAPLE, PINE};
+
+int main() {
+	int num = 99;
+	if (num > 10 && num < 100)
+		cout << "Acceptable\n";
+	int* numberz = new int[5];
+	for (int i = 0; i <= 4; i++)
+		*(numberz + i) = i;
+	cout << numberz[2] << endl;
+
+	char company[12] = "James Hayes";
+
+	Tree t1 = OAK;
+	Tree t2 = MAPLE;
+
+	if (OAK > PINE)
+		cout << "True" << endl;
+	else
+		cout << "False" << endl;
+	//int nums[5];
+	int numbers[4] = {99, 87};
+	cout << numbers[3] << endl;
+}
